8ccc5961a73b0239f2a22f7566abe691da12e5a2
Junior <jnrplay@hotmail.com>
2026-05-18 17:50:44 -0300

    feat: sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://comizy.com.br",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "daily",
       priority: 1,
     },
   ];
